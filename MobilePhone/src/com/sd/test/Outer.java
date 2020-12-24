46b4bbab8fc69015cb4c9adb5147d58b6fdac53f
sleeping-wind <842700325@qq.com>
2020-12-24 15:06:32 +0800

    //test

--- MobilePhone/src/com/sd/test/Outer.java
+++ MobilePhone/src/com/sd/test/Outer.java
@@ -1,9 +1,9 @@
 package com.sd.test;
 // test pull
 public class Outer {
-	private String s = "外部内私有属性";
+	private String s = "澶栭儴鍐呯鏈夊睘鎬�";
 	private void play() {
-		System.out.println("外部私有方法");
+		System.out.println("澶栭儴绉佹湁鏂规硶");
 	}
 	public class Inner{
 		void test(){
@@ -12,3 +12,4 @@ public class Outer {
 		}
 	}
 }
+// successful
