6e36d54e0ea0803bd39c204e9708c31e3f843849
sleeping-wind <842700325@qq.com>
2020-12-24 14:30:41 +0800

    成员内部类

---
+++ MobilePhone/src/com/sd/test/Outer.java
@@ -0,0 +1,16 @@
+package com.sd.test;
+
+
+public class Outer {
+	private String s = "外部内私有属性";
+	private void play() {
+		System.out.println("外部私有方法");
+	}
+	public class Inner{
+		void test(){
+			System.out.println(s);
+			play();
+		}
+		
+	}
+}
---
+++ MobilePhone/src/com/sd/test/T4est.java
@@ -0,0 +1,13 @@
+package com.sd.test;
+
+import com.sd.test.Outer.Inner;
+// 通过内部类调用外部类的私有方法，属性
+public class T4est {
+	public static void main(String[] args) {
+		Outer outer = new Outer();
+		
+		Inner inn = outer.new Inner();
+		inn.test();
+		
+	}
+}
